ed2f7b2e9db660034cb000faa66b1370ab66c370
Felix Wieland <mail@felixwie.com>
2020-10-24 15:21:29 +0200

    testing

--- test/test-volumes.js
+++ test/test-volumes.js
@@ -4,28 +4,30 @@ const HetznerCloud = require('../lib')
 const client = new HetznerCloud.Client(process.env.API_TOKEN)
 
 describe('VolumesEndpoint', function () {
-	let server = null
+	let test_server = null
 	let volume = null
 
 	before(async function () {
-		server = await client.servers
+		const { server } = await client.servers
 			.build('test-server')
 			.serverType('cx11')
-			.location('hel1')
+			.location('nbg1')
 			.image('ubuntu-20.04')
 			.create()
+
+		test_server = server
 	})
 
 	after(async function () {
-		await client.servers.delete(server.id)
+		await client.servers.delete(test_server.id)
 	})
 
 	describe('#build()', function () {
-		it('should create volume attached to server', async function () {
+		it('should create a volume', async function () {
 			const response = await client.volumes
 				.build('test_vol')
 				.size(10)
-				.server(server)
+				.server(test_server.id)
 				.create()
 			volume = response.volume
 			expect(response.volume.status).to.equal('creating')
@@ -36,14 +38,15 @@ describe('VolumesEndpoint', function () {
 		it('should detach volume from server', async function () {
 			const response = await volume.detach()
 
-			expect(response.status).to.be.oneOf(['success', 'running'])
+			expect(response.action.status).to.be.oneOf(['success', 'running'])
 		})
 	})
 
 	describe('#delete()', function () {
 		it('should delete volume', async function () {
-			const response = await client.volumes
-			expect(response.status).to.equal('success')
+			const response = await volume.delete()
+
+			expect(response.status).to.equal(204)
 		})
 	})
 })
